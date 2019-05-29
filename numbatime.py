d741a788f66a8f8017a12d49524c1096cf7ca58a
James Evans <milamber@clark-evans.com>
2019-05-29 07:05:13 -0500

    Make loops more equal

--- numbatime.py
+++ numbatime.py
@@ -19,16 +19,14 @@ def format_time(dt):
                 break
     return "%.*g %s" % (precision, dt / scale, unit)
 
-@jit("uint8(float64,float64,uint8)",nopython=True,cache=True)
-def mandel_core_float(creal, cimag, maxIter):
+@jit("uint8(float32,float32,uint8)",nopython=True,cache=True)
+def mandel_core_float32(creal, cimag, maxIter):
     zreal = 0
     zimag = 0
     #for i in range(maxIter):
-    i = 0
-    while i < maxIter:
-        i += 1
-        zreal_2 = zreal * zreal
-        zimag_2 = zimag * zimag
+    for i in range(maxIter):
+        zreal_2 = zreal**2
+        zimag_2 = zimag**2
         if zreal_2 + zimag_2 > 4:
             break
         zimag = 2 * zreal * zimag + cimag
@@ -38,19 +36,16 @@ def mandel_core_float(creal, cimag, maxIter):
     return i
 
 @jit("uint8(float64,float64,uint8)",nopython=True,cache=True)
-def mandel_core_float2(creal, cimag, maxIter):
+def mandel_core_float64(creal, cimag, maxIter):
     zreal = 0
     zimag = 0
-    #for i in range(maxIter):
-    i = 0
-    while i < maxIter:
-        i += 1
-        
-        tmp = zreal * zreal - zimag * zimag + creal
-        zimag = 2 * zreal * zimag + cimag
-        zreal = tmp
-        if zreal * zreal + zimag * zimag > 4:
+    for i in range(maxIter):
+        zreal_2 = zreal**2
+        zimag_2 = zimag**2
+        if zreal_2 + zimag_2 > 4:
             break
+        zimag = 2 * zreal * zimag + cimag
+        zreal = zreal_2 - zimag_2 + creal
         # (a + bi)(c + di) = ac + adi + bci - bd
         # (a + bi) + (c + di) = (a+c) + (b+d)i
     return i
@@ -78,14 +73,14 @@ if __name__ == "__main__":
     avg_time = float(bare_time / number)
     print("\tAverage execution took", format_time(avg_time))
 
-    print("Timing float funciton")
-    bare_time = timeit.timeit("mandel_core_float(creal, cimag, iter)", globals=globals(), number = number)
+    print("Timing float32 funciton")
+    bare_time = timeit.timeit("mandel_core_float32(creal, cimag, iter)", globals=globals(), number = number)
     print("\tExecution took %f" % bare_time)
     avg_time = float(bare_time / number)
     print("\tAverage execution took", format_time(avg_time))
 
-    print("Timing float2 funciton")
-    bare_time = timeit.timeit("mandel_core_float2(creal, cimag, iter)", globals=globals(), number = number)
+    print("Timing float64 funciton")
+    bare_time = timeit.timeit("mandel_core_float64(creal, cimag, iter)", globals=globals(), number = number)
     print("\tExecution took %f" % bare_time)
     avg_time = float(bare_time / number)
     print("\tAverage execution took", format_time(avg_time))
