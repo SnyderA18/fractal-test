b18be1d624973509296b99037a5bcf4fd4fa53bf
James Evans <milamber@clark-evans.com>
2019-05-30 16:46:56 -0500

    Add upgraded cython_core

--- cython_core.pyx
+++ cython_core.pyx
@@ -1,3 +1,4 @@
+import time
 cdef unsigned int _mandel_core_float(float creal, float cimag, unsigned int maxIter) nogil:
     cdef float zreal = 0.0
     cdef float zimag = 0.0
@@ -40,7 +41,13 @@ def mandel_core_complex_float(c, maxIter):
     return ret
 
 def mandel_core_complex_double(c, maxIter):
-    return _mandel_core_double(c.real, c.imag, maxIter)
+    cdef unsigned int ret
+    cdef float creal = c.real
+    cdef float cimag = c.imag
+    cdef unsigned int i = maxIter 
+    with nogil:
+        ret = _mandel_core_double(creal, cimag, i)
+    return ret
 
 def mandel_core_float(float creal, float cimag, unsigned int maxIter):
     cdef unsigned int ret
@@ -53,3 +60,78 @@ def mandel_core_double(double creal, double cimag, unsigned int maxIter):
     # with nogil:
     ret = _mandel_core_double(creal, cimag, maxIter)
     return ret
+
+def draw_fractal_double(double xmin, double xmax, double ymin, double ymax, 
+                    unsigned int maxIter, 
+                    unsigned int xoffset, unsigned int yoffset, unsigned int width, 
+                    unsigned int height, 
+                    unsigned int[:,:] buffer):
+    cdef double ystep = (ymax - ymin) / (height - 1)
+    cdef double xstep = (xmax - xmin) / (width - 1)
+    cdef unsigned int x,y
+    cdef double cx, cy
+    cdef unsigned int i
+
+    starttime = time.time()
+
+    for y in range(yoffset, yoffset + height):
+        cy = (y - yoffset) * ystep + ymin
+        for x in range(xoffset, xoffset + width):
+            cx = (x - xoffset) * xstep + xmin
+            # print("x,y =",(x,y), "cx,cy = (%0.4f,%0.4f)" % (cx,cy))
+            i = _mandel_core_double(cx, cy, maxIter)
+            if i >= maxIter - 1:
+                buffer[y,x] = 0
+            else:
+                color = (i << 21) + (i << 10) + i*8
+                buffer[y,x] = color
+    print("Calculation took %.2f seconds" % (time.time() - starttime))
+
+units = {"nsec": 1e-9, "usec": 1e-6, "msec": 1e-3, "sec": 1.0}
+precision = 3
+time_unit = None
+def format_time(dt):
+    unit = time_unit
+
+    if unit is not None:
+        scale = units[unit]
+    else:
+        scales = [(scale, unit) for unit, scale in units.items()]
+        scales.sort(reverse=True)
+        for scale, unit in scales:
+            if dt >= scale:
+                break
+    return "%.*g %s" % (precision, dt / scale, unit)
+
+def _draw_fractal_queue_double(command_queue, unsigned int[:,:] buffer):
+
+    cdef double ystep, xstep
+    cdef unsigned int x,y
+    cdef double cx, cy
+    cdef unsigned int i
+    cdef double xmin, xmax, ymin, ymax
+    cdef unsigned int maxIter, xoffset, yoffset, width, height
+
+    while True:
+        message = command_queue.get()
+        if message == "quit":
+            return
+
+        xmin, xmax, ymin, ymax, maxIter, xoffset, yoffset, width, height = message
+        ystep = (ymax - ymin) / (height - 1)
+        xstep = (xmax - xmin) / (width - 1)
+        starttime = time.time()
+
+        ystep = (ymax - ymin) / (height - 1)
+        xstep = (xmax - xmin) / (width - 1)
+        for y in range(yoffset, yoffset + height):
+            cy = (y - yoffset) * ystep + ymin
+            for x in range(xoffset, xoffset + width):
+                cx = (x - xoffset) * xstep + xmin
+                i = _mandel_core_double(cx, cy, maxIter)
+                if i >= maxIter - 1:
+                    buffer[y][x] = 0
+                else:
+                    color = ((i << 21) + (i << 10) + i<<3) & 0xffffff
+                    buffer[y][x] = color
+        print("Calculation took", format_time(time.time() - starttime))
